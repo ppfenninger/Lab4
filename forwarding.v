e3370292ebdafe63a70afe0435982ece3d8a7fa0
ppfenninger <paige.pfenninger@students.olin.edu>
2018-11-11 18:39:16 -0500

    fowarding now compilies

--- forwarding.v
+++ forwarding.v
@@ -1,10 +1,19 @@
-  module forwarding (
-  	input targetReg,
-  	input targetData,
-  	input MEM_reg,
-  	input MEM_data,
-  	input WB_reg,
-  	input WB_data
-  );
-  
-  endmodule
+module forwarding (
+	input targetReg,
+	input[31:0] targetData,
+	input[4:0] MEM_reg,
+	input[31:0] MEM_data,
+	input[4:0] WB_reg,
+	input[31:0] WB_data,
+	output reg[31:0] data
+);
+
+always @(targetData or targetReg) begin
+	case(targetReg)
+		MEM_reg: data=MEM_data;
+		WB_reg: data=WB_data;
+		default: data=targetData;
+	endcase // targetReg
+end
+
+endmodule
